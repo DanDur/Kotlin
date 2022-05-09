fa10ab7a11bf003113904188000bb7b765dbb7c5
DaniloDuran <daniloduran@hotmail.com>
2022-05-09 12:22:08 -0300

    implementando metodos saca e deposita

--- src/main/kotlin/bytebank.kt
+++ src/main/kotlin/bytebank.kt
@@ -1,7 +1,8 @@
 fun main() {
     println("Bem vindo ao Bytebank")
+
     val contaAlex = Conta()
-    contaAlex.titular="Alex"
+    contaAlex.titular = "Alex"
     contaAlex.numero = 1000
     contaAlex.saldo = 200.0
 
@@ -15,20 +16,74 @@ fun main() {
     println(contaFran.numero)
     println(contaFran.saldo)
 
-
     println(contaAlex.titular)
     println(contaAlex.numero)
     println(contaAlex.saldo)
+
+    println("depositando na conta do Alex")
+    contaAlex.deposita( 50.0)
+    println(contaAlex.saldo)
+
+    println("depositando na conta do Fran")
+    contaFran.deposita(70.0)
+    println(contaFran.saldo)
+
+    println("sacando na conta do Alex")
+    contaAlex.saca(250.0)
+    println(contaAlex.saldo)
+
+    println("sacando na conta da Fran")
+    contaFran.saca(100.0)
+    println(contaFran.saldo)
+
+    println("sacando em excesso na conta do Alex")
+    contaAlex.saca(100.0)
+    println(contaAlex.saldo)
+
+    println("sacando em excesso na conta da Fran")
+    contaFran.saca(500.0)
+    println(contaFran.saldo)
 }
 
-class Conta{
+
+
+class Conta {
     var titular = ""
     var numero = 0
     var saldo = 0.0
+
+    fun deposita(valor: Double) {
+        this.saldo += valor
+    }
+    fun saca(valor: Double){
+        if(saldo>=valor){
+            saldo-=valor
+        }
+    }
 }
 
+fun testaCopiasEReferencias() {
+    val numeroX = 10
+    var numeroY = numeroX
+    numeroY++
 
-fun testaLacos (){
+    println("numeroX $numeroX")
+    println("numeroY $numeroY")
+
+    val contaJoao = Conta()
+    contaJoao.titular = "João"
+    var contaMaria = Conta()
+    contaMaria.titular = "Maria"
+    contaJoao.titular = "João"
+
+    println("titular conta joao: ${contaJoao.titular}")
+    println("titular conta maria: ${contaMaria.titular}")
+
+    println(contaJoao)
+    println(contaMaria)
+}
+
+fun testaLacos() {
     var i = 0
     while (i < 5) {
         val titular: String = "Alex $i"
